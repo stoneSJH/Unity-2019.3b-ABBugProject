b4c2121bab7867a66e5c52b39a0eb27ac783c096
Stone Shi <shijiahao@unity3d.com>
2019-09-02 16:31:05 +0800

    init pj

---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateAssetBundle
+{
+    [MenuItem("Assets/Build AssetBundles/Windows")]
+    static void BuildAllAssetBundlesWindows()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+    }
+}
---
+++ Assets/Scenes/AssetBundleLoader.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class AssetBundleLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadSceneBundle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static public string bundleFolderName = "AssetBundles";
+    static int maxBundleIndex = 2;
+
+    void LoadSceneBundle()
+    {
+        // (1) load asset bundle
+        AssetBundle baseLoadedAssetBundle = null;
+        string path = Path.Combine(Application.dataPath, bundleFolderName);
+        for (int i = 0; i <= maxBundleIndex; i += 2)
+        {
+            var bundleName = string.Format("bundle{0}", i);
+            var bundlePath = Path.Combine(path, bundleName);
+            if (!File.Exists(bundlePath))
+                continue;
+
+            var bundle = AssetBundle.LoadFromFile(bundlePath);
+            if (null == bundle)
+            {
+                Debug.Log("Failed to load AssetBundle: " + bundlePath);
+            }
+
+            baseLoadedAssetBundle = bundle;
+        }
+
+
+        // (2) loaded scenes from asset bundle
+        string[] scenePath = baseLoadedAssetBundle.GetAllScenePaths();
+        foreach (var p in scenePath)
+        {
+            SceneManager.LoadScene(p, LoadSceneMode.Additive);
+            break;
+        }
+    }
+}
